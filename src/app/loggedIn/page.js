43af8014c1f7dcc5636fbfdb732b21ba1e84ff47
MbadaShehade <157805443+MbadaShehade@users.noreply.github.com>
2025-03-09 14:27:30 +0300

    eye icon password

--- src/app/loggedIn/page.js
+++ src/app/loggedIn/page.js
@@ -17,7 +17,6 @@ export default function LoggedInPage() {
       <main className="main-content">
         <button
           onClick={handleLogout}
-          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
           Logout
         </button>
