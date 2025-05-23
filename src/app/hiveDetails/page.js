22b34446995e2d8a7d707b04524288bea3107238
MbadaShehade <mbadashehade@gmail.com>
2025-05-24 00:42:56 +0300

    new2

--- src/app/hiveDetails/page.js
+++ src/app/hiveDetails/page.js
@@ -1,5 +1,6 @@
-export const dynamic = "force-dynamic";
 'use client';
+export const dynamic = "force-dynamic";
+
 
 import './hiveDetails.css';
 import React, { useState, useEffect, useRef } from 'react';
