22d245fe13ed927aa1c030be14237d785e6010a5
MbadaShehade <mbadashehade@gmail.com>
2025-05-24 00:54:40 +0300

    new4

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: "HiveGuard - Protect Your Hive With IOT",
