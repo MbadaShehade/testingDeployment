b29fe3552ed9c941eb132d63af44f2401c52c0f2
MbadaShehade <157805443+MbadaShehade@users.noreply.github.com>
2025-05-25 14:43:24 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-// import Footer from "./components/ServerComponents/Footer/Footer";
+import Footer from "./components/ServerComponents/Footer/Footer";
 import { fetchServerMQTTMonitor } from './_lib/mqtt-helpers';
 
 const geistSans = Geist({
@@ -23,7 +23,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   // Initialize MQTT monitoring service on server
-  // fetchServerMQTTMonitor();
   
   return (
     <html lang="en" suppressHydrationWarning>
@@ -64,7 +63,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="App">
             {children}
-            {/* <Footer /> */}
+            <Footer />
           </div>
         </ThemeProvider>    
       </body>   
