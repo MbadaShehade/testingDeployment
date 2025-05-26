be952e24a88974e586b1f3560a8ddb359bd55e4d
MbadaShehade <mbadashehade@gmail.com>
2025-05-26 13:59:26 +0300

    graph

--- src/app/components/ClientComponents/BeeHiveManagement/BeehiveManagement.js
+++ src/app/components/ClientComponents/BeeHiveManagement/BeehiveManagement.js
@@ -135,17 +135,19 @@ const BeehiveManagement = ({email, username, password, hiveGroups, setHiveGroups
         return;
       }
 
-      const client = mqtt.connect('ws://test.mosquitto.org:8080', {
+      const client = mqtt.connect('wss://test.mosquitto.org:8080', {
         clientId: `hiveguard_${Math.random().toString(16).substr(2, 8)}`,
         clean: true,
         reconnectPeriod: 0,
         connectTimeout: 30000, 
         keepalive: 60,       
         resubscribe: false,   
-        protocol: 'ws',       
+        protocol: 'wss',       
         qos: 1               
       });
 
+
+      
       let connectionTimeout = setTimeout(() => {
         if (client) {
           client.end(true);
